99da85495c31f379948bc48765d1d6f2f79eefd6
ssacog00 <ssacog00@estudiantes.unileon.es>
2020-05-23 17:50:04 +0200

    Añadido funcionalidad cliente (Lista de deseos completa)

---
+++ src/main/java/com/unileon/controller/ListaDeseosController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unileon.controller;
+
+import com.unileon.EJB.ListaDeseosFacadeLocal;
+import com.unileon.modelo.ListaDeseos;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Sergio
+ */
+@Named
+@ViewScoped
+
+public class ListaDeseosController implements Serializable{
+    
+    @EJB
+    private ListaDeseosFacadeLocal deseosEJB;
+    
+    private List<ListaDeseos> listaDeseos;
+    
+    
+    @PostConstruct
+    public void init() {
+        listaDeseos = deseosEJB.findAll();
+    }
+
+    public List<ListaDeseos> getListaDeseos() {
+        return listaDeseos;
+    }
+
+    public void setListaDeseos(List<ListaDeseos> listaDeseos) {
+        this.listaDeseos = listaDeseos;
+    }
+    
+    
+    public void eliminarDeseo(ListaDeseos deseo){
+        
+        try {
+            for(ListaDeseos d:listaDeseos){
+                if(d.getIdDeseos() == deseo.getIdDeseos()){
+                    deseo = d;
+                    break;
+                }
+            }
+            deseosEJB.remove(deseo);
+            this.actualizarTabla();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Deseo eliminado"));
+            
+        } catch(Exception e) {
+            System.out.println("Error al eliminar deseo: "+e.getMessage());
+        }
+        
+    } 
+    
+    private void actualizarTabla() {
+        listaDeseos = deseosEJB.findAll();
+    }
+}
--- src/main/java/com/unileon/controller/PantallaInicioClienteController.java
+++ src/main/java/com/unileon/controller/PantallaInicioClienteController.java
@@ -6,12 +6,17 @@
 package com.unileon.controller;
 
 
+import com.unileon.EJB.ListaDeseosFacadeLocal;
 import com.unileon.EJB.ProductoFacadeLocal;
+import com.unileon.modelo.ListaDeseos;
 import com.unileon.modelo.Producto;
+import com.unileon.modelo.Usuario;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -27,13 +32,22 @@ public class PantallaInicioClienteController implements Serializable{
     @EJB
     private ProductoFacadeLocal productoEJB;
     
+    @EJB
+    private ListaDeseosFacadeLocal deseosEJB;
+    
     private List<Producto> listaProductos;
     private Producto productoSeleccionado;
+    private ListaDeseos deseo;
+    private Usuario us;
+    private int enumValue;
     
     @PostConstruct
     public void init() {
+        us = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
         listaProductos = productoEJB.findAll();
-        
+        productoSeleccionado = new Producto();
+        deseo = new ListaDeseos();
+        enumValue = 0;
     }
 
     public List<Producto> getListaProductos() {
@@ -47,7 +61,28 @@ public class PantallaInicioClienteController implements Serializable{
     public void setProductoSeleccionado(Producto productoSeleccionado) {
         this.productoSeleccionado = productoSeleccionado;
     }
+
+    public int getEnumValue() {
+        return enumValue;
+    }
+
+    public void setEnumValue(int enumValue) {
+        this.enumValue = enumValue;
+    }
     
     
+    public void nuevoDeseo() {
+        try {
+            deseo.setCliente(us.getCliente());
+            deseo.setProducto(productoSeleccionado);
+            deseo.setValoracion(enumValue);
+            
+            deseosEJB.create(deseo);
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Deseo registrado"));
+        } catch(Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+    }
     
 }
